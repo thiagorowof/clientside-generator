2684feeb08c27a95fbec35f091010ab51ffb66e6
Mauricio Fernandes <mfernand@motorola.com>
2016-06-08 18:11:18 -0300

    criando estrutura inicial de pastas

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,17 +10,46 @@ module.exports = yeoman.Base.extend({
       'Welcome to the epic ' + chalk.red('generator-clientside') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        name: 'appName',
+        message: 'What is your app\'s name?'
+      },
+      {
+        name: 'appDescription',
+        message: 'Describe your app:'
+      },
+      {
+        name: 'appClient',
+        message: 'Client:'
+      },
+      {
+        name: 'appGit',
+        message: 'Github username or organization:'
+      }];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
     }.bind(this));
+  },
+
+  scaffoldFolders: function(){
+    this.mkdirp("app");
+    this.mkdirp("app/assets");
+    this.mkdirp("app/assets/fonts");
+    this.mkdirp("app/assets/img");
+    this.mkdirp("app/assets/js");
+    this.mkdirp("app/assets/styles");
+    this.mkdirp("app/modules");
+    this.mkdirp("app/modules/nomeModulo");
+    this.mkdirp("app/modules/nomeModulo/controllers");
+    this.mkdirp("app/modules/nomeModulo/factories");
+    this.mkdirp("app/modules/nomeModulo/i18n");
+    this.mkdirp("app/modules/nomeModulo/partials");
+    this.mkdirp("app/modules/nomeModulo/services");
+    this.mkdirp("app/modules/nomeModulo/views");
+    this.mkdirp("dist");
   },
 
   writing: function () {
