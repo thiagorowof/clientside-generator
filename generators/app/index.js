551a55f179014cb07b6facb236e3690c2c26bdc1
Dantas <thiago.de.a.dantas@accenture.com>
2016-06-09 14:07:05 -0300

    -Removed unusable files (.._doGenerator files) - Add first gruntfile.js - Add first bower.json - Add index html template - Changes on package.json and index.js( index merged prompts of mauricio user)

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,59 +4,89 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
-  prompting: function () {
+  prompting: function() {
+    var done = this.async();
+
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the epic ' + chalk.red('generator-clientside') + ' generator!'
     ));
 
-    var prompts = [
+    this.prompt([
       {
-        name: 'appName',
-        message: 'What is your app\'s name?'
+        type    : 'input',
+        name    : 'name',
+        message : 'What is your app\'s name?',
+        default : this.appname // Default to current folder name
       },
       {
-        name: 'appDescription',
-        message: 'Describe your app:'
+        type    : 'input',
+        name    : 'description',
+        message : 'Describe your app:'
       },
       {
-        name: 'appClient',
-        message: 'Client:'
+        type    : 'input',
+        name    : 'client',
+        message : 'Client\'s name:'
       },
       {
-        name: 'appGit',
-        message: 'Github username or organization:'
-      }];
-
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+        type    : 'input',
+        name    : 'git',
+        message : 'Github username or organization:'
+      },
+      {
+        type    : 'confirm',
+        name    : 'cool',
+        message : 'Would you like to enable the Cool feature?'
+      }]).then(function (answers) {
+      this.props = answers;
+      this.log('App\'s name:', answers.name);
+      this.log('Description:', answers.description);
+      this.log('Client\'s Name:', answers.client);
+      this.log('Git:', answers.git);
+      this.log('Cool feature:', answers.cool);
+      done();
     }.bind(this));
   },
 
-  scaffoldFolders: function(){
-    this.mkdirp("app");
-    this.mkdirp("app/assets");
-    this.mkdirp("app/assets/fonts");
-    this.mkdirp("app/assets/img");
-    this.mkdirp("app/assets/js");
-    this.mkdirp("app/assets/styles");
-    this.mkdirp("app/modules");
-    this.mkdirp("app/modules/nomeModulo");
-    this.mkdirp("app/modules/nomeModulo/controllers");
-    this.mkdirp("app/modules/nomeModulo/factories");
-    this.mkdirp("app/modules/nomeModulo/i18n");
-    this.mkdirp("app/modules/nomeModulo/partials");
-    this.mkdirp("app/modules/nomeModulo/services");
-    this.mkdirp("app/modules/nomeModulo/views");
-    this.mkdirp("dist");
-  },
+  // scaffoldFolders: function(){
+  //   this.mkdirp("app");
+  //   this.mkdirp("app/assets");
+  //   this.mkdirp("app/assets/fonts");
+  //   this.mkdirp("app/assets/img");
+  //   this.mkdirp("app/assets/js");
+  //   this.mkdirp("app/assets/styles");
+  //   this.mkdirp("app/modules");
+  //   this.mkdirp("app/modules/nomeModulo");
+  //   this.mkdirp("app/modules/nomeModulo/controllers");
+  //   this.mkdirp("app/modules/nomeModulo/factories");
+  //   this.mkdirp("app/modules/nomeModulo/i18n");
+  //   this.mkdirp("app/modules/nomeModulo/partials");
+  //   this.mkdirp("app/modules/nomeModulo/services");
+  //   this.mkdirp("app/modules/nomeModulo/views");
+  //   this.mkdirp("dist");
+  // },
 
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      {package: this.props.name}
     );
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('app/index.html'),
+      {title: this.props.description}
+    );
+    this.fs.copyTpl(
+      this.templatePath('bower.json'),
+      this.destinationPath('bower.json'),
+      {package: this.props.name}
+    );
+    // this.fs.copy(
+    //   this.templatePath('bowerrc'),
+    //   this.destinationPath('.bowerrc')
+    // );
   },
 
   install: function () {
