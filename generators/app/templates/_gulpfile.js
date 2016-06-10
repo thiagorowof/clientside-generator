8373ae21cbc40e9f5b4478a425767471db6daef1
Dantas <thiago.de.a.dantas@accenture.com>
2016-06-10 16:42:23 -0300

    -View/controller copytpl add to index.js -list of gulp plugins added to gulpfile

---
+++ generators/app/templates/_gulpfile.js
@@ -0,0 +1,17 @@
+gulp-uglify (minificar arquivos (js e e tal))
+gulp-rename(pra renomear e deixar tanto o original como o minificado a cima)
+gulp-less (converter less > css)
+gulp-sass (converer sass > css)
+gulp-watch (monitorar mudanças de arquivos)
+gulp-browser-sync (live reload nas paginas)
+gulp "lints" e "hints":
+  'css,js,html e json'
+gulp-csso (minificador css)
+gulp-jscs (padronizador js, padronizar usando o airbnb, recomendo)
+gulp-imagemin / gulp-tinypng (compressao de img)
+gulp-grunt - ( rodar tasks do grunt no gulp...util?)
+gulp-bower-files (injeta bower packages)
+
+
+
+https://github.com/vol7/shorthand simplifica muito CSSs!
